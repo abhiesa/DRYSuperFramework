d040769068fa089e0db10a2a1e1e45da8f7d5590
rognoni <rognoni@users.noreply.github.com>
2017-03-04 15:44:20 +0100

    Added "Test" folder

---
+++ Test/user_rest.py
@@ -0,0 +1,9 @@
+'''
+Manual test:
+
+curl -i -X POST -H 'Content-Type:application/json' -d '{"firstName":"Frodo","lastName":"Baggins"}' localhost:8080/users
+
+curl localhost:8080/users
+
+curl localhost:8080/users/1
+'''
