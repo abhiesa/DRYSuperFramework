6cb1d4aea603dc7ab27f6f447af7cdeba7b6c3c1
rognoni <rognoni@users.noreply.github.com>
2017-03-07 20:46:21 +0100

    Updating associated resource via REST
    http://stackoverflow.com/questions/22499819/spring-data-rest-update-a-re
    source%C2%B4s-association-using-proper-http-method

--- Test/user_rest.py
+++ Test/user_rest.py
@@ -6,4 +6,18 @@ curl -i -X POST -H 'Content-Type:application/json' -d '{"email":"fb@example.com"
 curl localhost:8080/users
 
 curl localhost:8080/users/1
+
+
+curl -i -X POST -H 'Content-Type:application/json' -d '{"name":"Admins"}' localhost:8080/groups
+curl -i -X POST -H 'Content-Type:application/json' -d '{"name":"Testers"}' localhost:8080/groups
+
+curl localhost:8080/groups
+
+curl localhost:8080/groups/1
+
+
+curl -v -X PUT -H "Content-Type: text/uri-list" -d "http://localhost:8080/groups/1" http://localhost:8080/users/1/groups
+
+curl -X PUT -H "Content-Type: application/json" -d '{"_links":{"groups":{"href":"http://localhost:8080/groups/1"}}}' http://localhost:8080/users/1/groups
+
 '''
