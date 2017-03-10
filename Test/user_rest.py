3a2dcf5a8f02d013380d5a71a44da804278872f1
rognoni <rognoni@users.noreply.github.com>
2017-03-10 14:56:56 +0100

    Added "requests"

--- Test/user_rest.py
+++ Test/user_rest.py
@@ -7,13 +7,46 @@ curl -i -X POST -H 'Content-Type:application/json' -d '{"name":"Testers"}' local
 curl localhost:8080/groups
 curl localhost:8080/groups/1
 
-#curl -v -X PUT -H "Content-Type: text/uri-list" -d "http://localhost:8080/groups/1" http://localhost:8080/users/1/groups
-#curl -X PUT -H "Content-Type: application/json" -d '{"_links":{"groups":{"href":"http://localhost:8080/groups/1"}}}' http://localhost:8080/users/1/groups
-
 curl -i -X POST -H 'Content-Type:application/json' -d '{"email":"fb@example.com","firstName":"Frodo","lastName":"Baggins"}' localhost:8080/users
 curl -i -X POST -H 'Content-Type:application/json' -d '{"email":"fb@example.com","firstName":"Frodo","lastName":"Baggins","groups":["http://localhost:8080/groups/1","http://localhost:8080/groups/2"]}' localhost:8080/users
 
 curl localhost:8080/users
 curl localhost:8080/users/1
 
+#curl -v -X PUT -H "Content-Type: text/uri-list" -d "http://localhost:8080/groups/1" http://localhost:8080/users/1/groups
+#curl -X PUT -H "Content-Type: application/json" -d '{"_links":{"groups":{"href":"http://localhost:8080/groups/1"}}}' http://localhost:8080/users/1/groups
+
 '''
+
+import json
+
+import requests
+
+HEADERS_JSON = {'Content-Type': 'application/json'}
+
+def printj(j):
+    print(json.dumps(j, indent=4))
+
+def test_all():
+    requests.post('http://localhost:8080/groups', headers=HEADERS_JSON, data='{"name":"Admins"}')
+    requests.post('http://localhost:8080/groups', headers=HEADERS_JSON, data='{"name":"Testers"}')
+
+    rj = requests.get('http://localhost:8080/groups/').json()
+    groups = rj['_embedded']['groups']
+
+    groups_hrefs = []
+    for group in groups:
+        groups_hrefs.append(group['_links']['group']['href'])
+
+    user_json = {
+        "email": "fb@example.com",
+        "firstName": "Frodo",
+        "lastName": "Baggins",
+        "groups": groups_hrefs
+    }
+
+    r = requests.post('http://localhost:8080/users', headers=HEADERS_JSON, data=json.dumps(user_json))
+    print r.text
+
+if __name__ == '__main__':
+    test_all()
